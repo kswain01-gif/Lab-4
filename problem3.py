e17d70882ca56cc323bb2784ed12ede2765e3c84
kswain01-gif <kswain01@saintmarys.edu>
2026-09-14 16:59:29 -0400

    problem 3 solved

--- problem3.py
+++ problem3.py
@@ -1,14 +1,14 @@
 def pythagorean_triples(a,b,c):
   """
   number number number -> boolean
-  takes in three parameters a,b,c and returns True if a^2+b^2=c^2 and False otherwise
+  takes in three parameters a,b,c and returns True if a**2+b**2=c**2 and False otherwise
 
-  >>> pythagorean_triplets(2,4,5):
+  >>> pythagorean_triples(2,4,5)
   False
-  >>> pythagorean_triplets(2,0,2):
+  >>> pythagorean_triples(2,0,2)
   True
-  """ 
-  if a**2+b**2==c^2:
+  """
+  if a**2+b**2==c**2:
     return True
-  else: 
+  else:
     return False
