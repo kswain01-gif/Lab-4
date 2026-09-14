9ac857c5b0dc12dfc023cf4ddde2cf298d18f509
kswain01-gif <kswain01@saintmarys.edu>
2026-09-14 16:45:33 -0400

    Add pythagorean_triples function implementation
    Implement pythagorean_triples function to check if a^2 + b^2 equals c^2.

--- problem3.py
+++ problem3.py
@@ -1,2 +1,14 @@
 def pythagorean_triples(a,b,c):
-  ## your code here
+  """
+  number number number -> boolean
+  takes in three parameters a,b,c and returns True if a^2+b^2=c^2 and False otherwise
+
+  >>> pythagorean_triplets(2,4,5):
+  False
+  >>> pythagorean_triplets(2,0,2):
+  True
+  """ 
+  if a**2+b**2==c^2:
+    return True
+  else: 
+    return False
