1f541f54f504a0d73156cc8950cf0ba2d2761312
kswain01-gif <kswain01@saintmarys.edu>
2026-09-20 16:58:44 -0400

    Update problem6.py

--- problem6.py
+++ problem6.py
@@ -39,10 +39,10 @@ def with_if_function():
   return if_function(c(), t(), f())
 
 def c():
-  ### you write this
+  return True
 
-def t():
-  ### you write this
+def 
+  print (1)
 
 def f():
-  ### you write this
+  print (2)
