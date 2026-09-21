c7d3e200325bb0f799d4e8dbced5cf9de35b4a02
kswain01-gif <kswain01@saintmarys.edu>
2026-09-21 10:14:11 -0400

    problem 4 solved
    Implement function to check if three lengths can form a triangle.

--- problem4.py
+++ problem4.py
@@ -1,2 +1,14 @@
 def is_triangle(a,b,c):
-  ## your turn
+  """
+  number number number -> boolean
+  takes three numbers a,b,c as input and returns True if these numbers can be the side lengths of a triangle and False otherwise
+
+  >>> is_triangle(3,4,5)
+  True
+  >>> is_triangle(3,6,9)
+  False
+  """
+  if a+b>c:
+    return True
+  else:
+    return False
